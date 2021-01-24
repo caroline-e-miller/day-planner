97d1da3fbcb1f6967eb9d54e16f9321037aa79ab
caroline-e-miller <caroline.miller@ymail.com>
2021-01-23 20:40:44 -0600

    established rows on page, wrote script for local storage

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-// connect moment.js
 // establish variables
+var present;
+var past;
+var future;
+var textInput;
 // if time is equal to present, row style is red
 // if time is lesser than present, row style is grey
 // if time is greater than present, row style is green
 // set up input 
 // set up local storage for text which is dependent upon the save button click event
+$(".saveBtn").click(function () {
+    var plans = $("#input");
+    var bigPlans = plans.text();
+    console.log(bigPlans)
+
+    localStorage.setItem("bigPlans", JSON.stringify(bigPlans));
+})
