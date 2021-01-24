0159036fc87adcfffa4c722c343789485723c4fd
caroline-e-miller <caroline.miller@ymail.com>
2021-01-23 19:21:33 -0600

    initial push and pseudocode

---
+++ script.js
@@ -0,0 +1,7 @@
+// connect moment.js
+// establish variables
+// if time is equal to present, row style is red
+// if time is lesser than present, row style is grey
+// if time is greater than present, row style is green
+// set up input 
+// set up local storage for text which is dependent upon the save button click event
